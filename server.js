af8c158e4bdc27338b8d8455e3687e75da334f2d
Andrew Garcia <andrew@drwgrc.dev>
2022-03-18 11:58:08 -0400

    setup

---
+++ server.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+const helmet = require('helmet');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(helmet());
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; // TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: 'You must log in!',
+    });
+  }
+  next();
+}
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+  return res.send('Your personal secret value is 42!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+  });
